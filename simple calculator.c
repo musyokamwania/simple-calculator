f1a7af4369e1e5f08d2c5990cc9bc054784105a0
musyokamwania <99266001+musyokamwania@users.noreply.github.com>
2022-02-15 17:01:29 +0300

    Add files via upload

---
+++ simple calculator.c
@@ -0,0 +1,39 @@
+
+
+#include <stdlib.h>
+int main()
+{
+    int num1,num2,sum,diff,pro,mod;
+    float qout;
+
+    //assignment
+    num1 =200;
+    num2 =56;
+
+    //computation
+    sum =num1 + num2;
+    diff = num1 - num2;
+    pro = num1 / num2;
+    mod = num1 % num2;
+
+    //output
+
+    //sum: result
+    // 3 + 5 = 8
+    printf("%d + %d = %d/n",num1,num2,sum);
+    printf("%d + %d = %d/n",num1,num2,diff);
+    printf("%d + %d = %d/n",num1,num2,pro);
+    printf("%d + %d = %d/n",num1,num2,quot);
+    printf("%d + %d = %d/n",num1,num2,mod);
+
+
+
+
+
+    return 0;
+
+
+
+    printf("Hello world!\n");
+    return 0;
+}
